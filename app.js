2fac77a80a48915dd96f2ee25339ae47b5fbe01e
asimonw <awijns@gmail.com>
2016-07-26 21:12:41 +0100

    Command-line app: Add get and post methods

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-console.log("Reading List:");
+// usage: node app.js [method params]
+// method: 'get' or 'post'
+// params: id for get
+//         author and title for post
+
+var argv = process.argv;
+
+// 'node app.js' runs get by default
+var method = (argv.length > 2) ? argv[2] : 'get';
 
 var books = [
   {
@@ -12,13 +20,51 @@ var books = [
     title: "Zen And The Art Of Motorcycle Maintenance"
   }
 ];
+// needed to make sure id is unique when posting
+var bookItems = 2;
 
-// imperative style
-// for (var i = 0; i < books.length; i++) {
-//   console.log(books[i].author + " - " + books[i].title);
-// }
+switch (method) {
+  case 'get':
+    get();
+    break;
+  case 'post':
+    post();
+    break;
+  default:
+    console.log('Instruction not recognised');
+}
 
-// functional style
-books.forEach(function (book) {
-  console.log(book.author + " - " + book.title);
-});
+// return all books, or one book with specified id
+function get() {
+  var filteredBooks = books;
+  if (argv.length > 3) {
+    var item = argv[3];
+    filteredBooks = books.filter(function (book) {
+      return book.id === parseInt(item, 10);
+    });
+    if (filteredBooks.length === 0) {
+      console.log('No book with that id');
+      return;
+    }
+  } else {
+    console.log("Reading List:");
+  }
+  filteredBooks.forEach(function (book) {
+    console.log(book.author + " - " + book.title);
+  });
+}
+
+// add new book
+// problem: change does not persist --> add file storage
+function post() {
+  if (argv.length > 4) {
+    books.push({
+      id: bookItems++,
+      author: argv[3],
+      title: argv[4]
+    });
+    console.log('Book added to list');
+  } else {
+    console.log('Error: insufficient number of arguments');
+  }
+}
