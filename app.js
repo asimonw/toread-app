a578243ff71dfc9cc3454ef39b2c7972b5de2a35
asimonw <awijns@gmail.com>
2016-07-31 17:58:31 +0100

    Add simple file based storage using JSON

--- app.js
+++ app.js
@@ -2,26 +2,14 @@
 // method: 'get' or 'post'
 // params: id for get
 //         author and title for post
+var fs = require('fs');
+var path = require('path');
 
 var argv = process.argv;
+var BOOKS_FILE = path.join(__dirname, 'books.json');
 
 // 'node app.js' runs get by default
 var method = (argv.length > 2) ? argv[2] : 'get';
-
-var books = [
-  {
-    id: 0,
-    author: "Douglas R. Hofstadter",
-    title: "Gödel, Escher, Bach"
-  },
-  {
-    id: 1,
-    author: "Robert Pirsig",
-    title: "Zen And The Art Of Motorcycle Maintenance"
-  }
-];
-// needed to make sure id is unique when posting
-var bookItems = 2;
 
 switch (method) {
   case 'get':
@@ -36,34 +24,60 @@ switch (method) {
 
 // return all books, or one book with specified id
 function get() {
-  var filteredBooks = books;
-  if (argv.length > 3) {
-    filteredBooks = books.filter(function (book) {
-      return book.id === parseInt(argv[3], 10);
-    });
-    if (filteredBooks.length === 0) {
-      console.log("No book with that id");
-      return;
+  fs.readFile(BOOKS_FILE, function (error, data) {
+    if (error) {
+      console.error(error);
+      process.exit(1);
     }
-  } else {
-    console.log("Reading List:");
-  }
-  filteredBooks.forEach(function (book) {
-    console.log(book.author + " - " + book.title);
+
+    var books = JSON.parse(data);
+    var filteredBooks = books;
+
+    if (argv.length > 3) {
+      filteredBooks = books.filter(function (book) {
+        return book.id === parseInt(argv[3], 10);
+      });
+      if (filteredBooks.length === 0) {
+        console.log("No book with that id");
+        process.exit(0);
+      }
+    } else {
+      console.log("Reading List:");
+    }
+    filteredBooks.forEach(function (book) {
+      console.log(book.author + " - " + book.title);
+    });
   });
 }
 
 // add new book
-// problem: change does not persist --> add file storage
 function post() {
-  if (argv.length > 4) {
+  if (argv.length < 5) {
+    console.log("Error: insufficient number of arguments");
+    process.exit(1);
+  }
+  fs.readFile(BOOKS_FILE, function (error, data) {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+
+    var books = JSON.parse(data);
+    var bookIds = books.map(function (book) {
+      return book.id;
+    });
+    var maxId = Math.max.apply(Math, bookIds);
     books.push({
-      id: bookItems++,
+      id: maxId + 1,
       author: argv[3],
       title: argv[4]
     });
-    console.log("Book added to list");
-  } else {
-    console.log("Error: insufficient number of arguments");
-  }
+    fs.writeFile(BOOKS_FILE, JSON.stringify(books, null, 2), function (error) {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+      console.log("Book added to list");
+    });
+  })
 }
