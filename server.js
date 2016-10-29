3a7ec2765893e28097c4a7e611e65ac5249a423c
asimonw <awijns@gmail.com>
2016-10-29 19:57:21 +0100

    Add get method - step 1

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+var BOOKS_FILE = path.join(__dirname, 'books.json');
 
 var server = http.createServer(function (req, res) {
   var path = url.parse(req.url).path;
-  var result;
   switch (path) {
     case '/':
-      result = 'homepage';
+      res.end('<h1>homepage</h1>');
       break;
-    case '/about':
-      result = 'about';
+    case '/books':
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      fs.readFile(BOOKS_FILE, function (error, data) {
+        var result = {};
+        if (error) {
+          console.error(error);
+          result.success = false;
+          result.errorMessage = "Unable to get books";
+          result.data = null;
+        } else {
+          var books = JSON.parse(data);
+          result.success = true;
+          result.errorMessage = "";
+          result.data = books;
+        }
+        res.end(JSON.stringify(result));
+      });
       break;
     default:
-      result = 'not found';
+      res.end('not found');
   }
-  res.end(result);
 });
 
 server.listen(8080, function () {
