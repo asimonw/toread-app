da4b07d64bc0928e95f7e3513de141d53b98277f
asimonw <awijns@gmail.com>
2016-11-06 22:13:31 +0000

    Start using express

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
 var fs = require('fs');
 var path = require('path');
 
+var app = express();
+
 var BOOKS_FILE = path.join(__dirname, 'books.json');
 
-var server = http.createServer(function (req, res) {
-  var path = url.parse(req.url).path;
-  switch (path) {
-    case '/':
-      res.end('<h1>homepage</h1>');
-      break;
-    case '/books':
-      res.setHeader('Content-Type', 'application/json; charset=utf-8');
-      fs.readFile(BOOKS_FILE, function (error, data) {
-        var result = {};
-        if (error) {
-          console.error(error);
-          result.success = false;
-          result.errorMessage = "Unable to get books";
-          result.data = null;
-        } else {
-          var books = JSON.parse(data);
-          result.success = true;
-          result.errorMessage = "";
-          result.data = books;
-        }
-        res.end(JSON.stringify(result));
-      });
-      break;
-    default:
-      res.end('not found');
-  }
+app.get('/books', function (req, res) {
+  fs.readFile(BOOKS_FILE, function (error, data) {
+    var result = {};
+    if (error) {
+      console.error(error);
+      result.success = false;
+      result.errorMessage = "Unable to get books";
+      result.data = null;
+    } else {
+      var books = JSON.parse(data);
+      result.success = true;
+      result.errorMessage = "";
+      result.data = books;
+    }
+    res.json(result);
+  });
 });
 
-server.listen(8080, function () {
+var server = app.listen(8080, function () {
   console.log("Listening on port " + server.address().port);
 });
