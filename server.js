becb972af19294dee14f88a74d47b9cd28c4532c
asimonw <awijns@gmail.com>
2016-11-10 21:34:48 +0000

    Add get method for single item

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash');
 
 var app = express();
 
 var BOOKS_FILE = path.join(__dirname, 'books.json');
 
-app.get('/books', function (req, res) {
-  fs.readFile(BOOKS_FILE, function (error, data) {
-    var result = {};
-    if (error) {
-      console.error(error);
-      result.success = false;
-      result.errorMessage = "Unable to get books";
-      result.data = null;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+function getBooks(req, res, next) {
+  fs.readFile(BOOKS_FILE, function (err, data) {
+    if (err) {
+      console.error(err);
+      req.books = null;
     } else {
-      var books = JSON.parse(data);
-      result.success = true;
-      result.errorMessage = "";
-      result.data = books;
+      req.books = JSON.parse(data);
     }
-    res.json(result);
+    next();
   });
+}
+
+app.get('/books', getBooks, function (req, res) {
+  var books = req.books;
+  if (books) {
+    res.json(books);
+  } else {
+    res.json([]);
+  }
+});
+
+app.get('/books/:id', getBooks, function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  var books = req.books;
+  if (books) {
+    var book = _.find(books, { id: id});
+    res.json(book || {});
+  } else {
+    res.json({});
+  }
 });
 
 var server = app.listen(8080, function () {
