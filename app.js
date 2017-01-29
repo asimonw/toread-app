955b2da4521c9391cdd3eebcabdc19009f00ffd9
asimonw <awijns@gmail.com>
2017-01-29 21:24:43 +0000

    Set up mongoose

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 var app = require('./server');
 
-var server = app.listen(8080, function () {
-  console.log("Listening on port " + server.address().port);
+var mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/ToreadApp');
+
+var BookSchema = {
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+};
+
+var Book = mongoose.model('Book', BookSchema);
+var toread = new Book({
+  author: 'Nick Bostrom',
+  title: 'Superintelligence'
 });
+
+toread.save()
+  .then(function (doc) {
+    console.log('Book saved', JSON.stringify(doc, undefined, 2));
+  })
+  .catch(function (e) {
+    console.log('Unable to save book', e);
+  });
+
+// var server = app.listen(8080, function () {
+//   console.log("Listening on port " + server.address().port);
+// });
