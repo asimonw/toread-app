7d1388d0d921918169074759de36113b84178773
asimonw <awijns@gmail.com>
2017-07-16 18:25:40 +0200

    Add delete book action to front-end

--- public/src/js/components/Book.js
+++ public/src/js/components/Book.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 class Book extends React.Component {
   render() {
-    let {author, title} = this.props.item
+    let {author, title, _id} = this.props.item
+    console.log(_id);
     return (
-      <li>{author}: {title}</li>
+      <li>
+        {author}: {title}
+        <span
+          id={_id}
+          className="book-delete"
+          onClick={this.props.handleDelete}> -</span>
+      </li>
     )
   }
 }
--- public/src/js/components/BookList.js
+++ public/src/js/components/BookList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import _ from 'lodash'
 
 import Book from './Book'
 
@@ -10,6 +11,7 @@ class BookList extends React.Component {
       books: []
     }
   }
+
   componentDidMount () {
     axios.get('/books')
       .then(response => this.setState({ books: response.data.books }))
@@ -17,12 +19,31 @@ class BookList extends React.Component {
       .catch(err => console.error(err))
   }
 
+  handleDelete(e) {
+    let _id = e.target.id;
+    axios.delete('/books/' + _id)
+      .then(response => {
+        let updatedBooks = _.filter(this.state.books, book => book._id !== _id)
+        this.setState({ books: updatedBooks})
+      })
+      .catch(err => console.error(err))
+  }
+
   render () {
     return (
       <div className="booklist">
         <ul>
-          {this.state.books.map(book => <Book key={book._id} item={book} />)}
+          {this.state.books.map(
+            book => <Book key={book._id} item={book}
+              handleDelete={this.handleDelete.bind(this)} />
+          )}
         </ul>
+        <form action="/books" method="post">
+          <h2>Add a book</h2>
+          <input name="author" />
+          <input name="title" />
+          <button type="submit">Add</button>
+        </form>
       </div>
     )
   }
